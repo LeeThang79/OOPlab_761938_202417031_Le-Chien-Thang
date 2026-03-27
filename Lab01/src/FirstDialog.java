d7767a67a9d49e4d10bad2f497d4b76b484cd374
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-03-27 17:01:27 +0700

    Assignment2.2 Completed

---
+++ Lab01/src/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] var0) {
+        JOptionPane.showMessageDialog(null, "Hello World\nHow are you!");
+        System.exit(0);
+    }
+}
