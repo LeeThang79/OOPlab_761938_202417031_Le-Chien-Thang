2f373bacead2b0d0c739627346ea97ff46235945
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-04-03 17:29:55 +0700

    Add Getter/Setter P9

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -5,4 +5,23 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
 }
