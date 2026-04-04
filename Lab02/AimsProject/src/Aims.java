2333af9d3837366e1807e19466f76c28d3f7c80b
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-04-04 16:13:17 +0700

    Add Create Carts

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -1,5 +1,20 @@
 public class Aims {
     public static void main(String[] args) {
-        //TODO Auto_generated method stub
+        // Create a new cart
+        Cart anOrder = new Cart();
+
+        // New dvd object and add it to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        // total cost
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
     }
 }
