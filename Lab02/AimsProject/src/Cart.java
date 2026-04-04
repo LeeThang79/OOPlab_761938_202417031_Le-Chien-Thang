6079318e9c83ac7498cde22b083f2867ad085b70
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-04-04 16:52:56 +0700

    Add Overloading Method P14

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -4,30 +4,31 @@ public class Cart {
     private int qtyOrdered = 0;
 
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
-            System.out.println("The disc has been added");
+            System.out.println(disc.getTitle() + " has been added!");
         }
-        else
+        else {
             System.out.println("The cart is almost full");
-
-    }
-
-    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-        for(int i = 0; i<qtyOrdered; i++) {
-            if(itemsOrdered[qtyOrdered] == disc) {
-                for(int j=i;j<qtyOrdered-1;j++) {
-                    itemsOrdered[j] = itemsOrdered[j+1];
-                }
-                itemsOrdered[qtyOrdered-1]=null;
-                qtyOrdered--;
-                System.out.println("The disc has been removed from cart");
-                return;
-            }
         }
     }
 
+    //Overloading differing types
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (int i = 0; i < dvdList.length; i++) {
+            DigitalVideoDisc disc = dvdList[i];
+            addDigitalVideoDisc(disc);
+        }
+    }
+
+    //Overloading differing number
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
+    }
+
+    // total cost
     public float totalCost() {
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
