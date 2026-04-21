68074f2d53a0bc30dc86155977dfaa40b256fb2c
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-04-21 15:29:54 +0700

    Feature: 10.1 - Disc class extending Media class

--- AimsProject/src/hust/soict/hedspi/aims/media/Media.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -37,4 +37,13 @@ public abstract class Media {
     public void setCost(float cost) {
         this.cost = cost;
     }
+
+    public Media() {
+
+    }
+    public Media(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
 }
