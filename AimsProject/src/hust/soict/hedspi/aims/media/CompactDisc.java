d1c655f3798e95a6ab71644f6dc1e2dc288bda94
LeeThang79 <THANG.LC2417031@sis.hust.edu.vn>
2026-04-21 16:14:21 +0700

    Feature: Completed CompactDisc class

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -0,0 +1,45 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc {
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<>();
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public CompactDisc(String title, String category, String artist, String director, int length, float cost) {
+        super(title,category,director,length,cost);
+        this.artist = artist;
+    }
+
+    public void addTrack(Track trackName) {
+        if(!tracks.contains(trackName)) {
+            tracks.add(trackName);
+            System.out.println("Added track " + trackName.getTitle() + " to the Disc");
+        }
+        else {
+            System.out.println("Track " +trackName.getTitle() + " has already existed in the Disc");
+        }
+    }
+
+    public void removeTrack (Track trackName) {
+        if(tracks.contains(trackName)) {
+            tracks.remove(trackName);
+            System.out.println("Track " + trackName.getTitle() + " has been removed from the Disc");
+        }
+        else {
+            System.out.println("Track " + trackName.getTitle() + " is not in the the Disc. Can't be removed.");
+        }
+    }
+
+    public int getLength () {
+        int totalLength = 0;
+        for(Track track:tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+}
